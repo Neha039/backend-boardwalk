34dae4c105272d968227a04a508c79f3fb15d163
YI YING <supercarboy88@gmail.com>
2020-01-10 00:23:08 -0800

    finalized user models file

--- models/user.js
+++ models/user.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 5,
+        trim: true,
+        // unique username
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
