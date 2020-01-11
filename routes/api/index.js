24f92baac06ff91c069c04e78870d0951b74dc1a
Neha Nehru <neha_nehru@hotmail.com>
2020-01-11 12:50:51 -0800

    made changes to models, routes, controllers folders to add profile files

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./users");
 const authRoutes = require("./auth");
+const profileRoutes = require("./profile");
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/profile", profileRoutes);
 
 module.exports = router;
