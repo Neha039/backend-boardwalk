7358d8fa3daa094ac480aee714b05c3d0dc69a27
YI YING <supercarboy88@gmail.com>
2020-01-12 10:04:25 -0800

    modified users.js and created protected.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,22 @@
 const router = require("express").Router();
-
 const userRoutes = require("./users");
-const authRoutes = require("./auth");
+const protectedRoutes = require("./protected");
 
+// Protected routes
+router.use("/protected", protectedRoutes);
+
+// User routes
 router.use("/users", userRoutes);
-router.use("/auth", authRoutes);
 
 module.exports = router;
+
+
+// const router = require("express").Router();
+
+// const userRoutes = require("./users");
+// const authRoutes = require("./auth");
+
+// router.use("/users", userRoutes);
+// router.use("/auth", authRoutes);
+
+// module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,21 +1,37 @@
-const authUtils = require('../../utils/authMethods');
 const router = require("express").Router();
-const usersController = require("../../controllers/usersController");
+const userController = require("../../controllers/userController");
 
 // Matches with "/api/users"
-router.route("/")
-  .get(usersController.findAll);
+ router.route("/")
+  .get(userController.findAll)
+  .post(userController.create); 
 
 // Matches with "/api/users/:id"
-router
+ router
   .route("/:id")
-  .get(usersController.findById);
-
-// Matches with "api/users/find/currentUser"
-router
-  .route("/find/currentUser")
-  .get(authUtils.validateLogin, usersController.findCurrentUser)
-  .put(authUtils.validateLogin, usersController.update)
-  .delete(authUtils.validateLogin, usersController.remove);
+  .get(userController.findById);
 
 module.exports = router;
+
+
+// const authUtils = require('../../utils/authMethods');
+// const router = require("express").Router();
+// const usersController = require("../../controllers/usersController");
+
+// // Matches with "/api/users"
+// router.route("/")
+//   .get(usersController.findAll);
+
+// // Matches with "/api/users/:id"
+// router
+//   .route("/:id")
+//   .get(usersController.findById);
+
+// // Matches with "api/users/find/currentUser"
+// router
+//   .route("/find/currentUser")
+//   .get(authUtils.validateLogin, usersController.findCurrentUser)
+//   .put(authUtils.validateLogin, usersController.update)
+//   .delete(authUtils.validateLogin, usersController.remove);
+
+// module.exports = router;
