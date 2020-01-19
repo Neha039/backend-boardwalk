8128e7f9e7c4bf37e9b30a724dbfd64c737555de
Neha Nehru <neha_nehru@hotmail.com>
2020-01-18 18:44:34 -0800

    made changes to debtController file

---
+++ controllers/debtController.js
@@ -0,0 +1,58 @@
+//Require our user model
+const Debt = require("../models/debt");
+
+module.exports = {
+    findAll: async function (req, res) {
+        try {
+            const results = await Debt.find({});
+            res.status(200).json(results);
+        }
+        catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    findById: async function (req, res) {
+        try {
+            const result = await User.findById(req.params.id);
+            if (!result) {
+                return res.sendStatus(404); //we didn't actually find anybody!
+            }
+            res.status(200).json(result);
+        }
+        catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    create: async function (req, res) {
+        try {
+            //create a new user!
+            //BUT FIRST!  validation (8
+            const { firstName, lastName, email, password } = req.body;
+            if (typeof email !== "string" || email === "") {
+                return res.status(400).json({ error: "Must provide an email address!" });
+            }
+
+            if (typeof password !== "string" || password.length < 8 || password.length > 64) {
+                return res.status(400).json({ error: "Must provide a password (8-64 characters)!" });
+            }
+
+            //Now that we have valid input, we have to protect our password
+            //(Note: this work can also live in the user schema; it is shown here so that we can trace what's going on)
+            const salt = await bcrypt.genSalt(10);
+            const saltedAndHashedPwd = await bcrypt.hash(password, salt);
+
+            //Finally, we create the new user:
+            const newUser = await User.create({ firstName: firstName, lastName: lastName, email: email, password: saltedAndHashedPwd });
+            
+            //And now, we log them in!
+            req.login(newUser, err => {
+                if(err) { throw err };
+                res.json(req.user); 
+            });
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+};
